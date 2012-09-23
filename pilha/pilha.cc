8736e871b09d6ad4d16a30558b2910b924298c98
Maurilio Atila <cabelotaina@gmail.com>
2012-09-23 15:18:22 -0300

    documentaçao pilha.cc na metade

--- pilha/pilha.cc
+++ pilha/pilha.cc
@@ -1,7 +1,61 @@
-/*
-@author Maurilio Atila;
-@date ...
-*/
+// FILE: pilha.cc
+// TITLE: Pilha
+// SUBMITTED BY: Maurilio Atila
+// Todo o código é meu, excepto quando creditado a outros.
+// FOR COURSE: INE5408
+// DUE DATE: 23 de Setembro 2012
+//
+// PURPOSE:
+// Este programa implementa uma pilha, funciona exatamente como
+// uma pilha do mundo real, onde podemos inserir elementos apenas
+// no topo, e podemos retirar os elementos apenas do topo. Ou seja
+// esta sobre o modelo FIFO - First in First out.
+//
+// OVERALL METHOD:
+// A lista geral de tarefas é:
+// 
+// 1. Entre com valores, ate o maximo de 30 valores.
+// 2. Liste os valores.
+// 3. Veja o valor que esta no topo.
+// 4. Remova valores da pilha.
+//
+// FUNCTIONS:
+//
+// insere
+// empilha um conteudo e retona o ultimo, em caso de pilha cheia retorna  
+// o codigo de erro e uma mensagem de pilha cheia.
+// 
+// remove
+// desempilha um elemento do topo e retorna o ultimo, em caso de pilha 
+// vazia 
+//
+// topoPilha
+//
+//
+//
+// inicializa
+//
+//
+//
+// mostra
+//
+//
+//
+// cheia
+//
+//
+//
+// vazia
+//
+//
+//
+// INCLUDED FILES:
+// string.h
+// stdio.h
+// pilha.h
+//
+// DATA FILES:
+// none
 
 #include <string.h>
 #include "pilha.h"
