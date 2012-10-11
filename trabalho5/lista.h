3242aa09f1db8683f707c5eb41952244a9ff2bce
Lauro Costa G. Borges <nhelauro@gmail.com>
2012-10-11 17:10:49 -0300

    divisao do trabalho e esboco geral

---
+++ trabalho5/lista.h
@@ -0,0 +1,40 @@
+Lista*  criaLista(){
+
+Booleano  listaVazia(Lista *aLista)
+
+
+int  adicionaNoInicio(Lista *aLista,TipoInfo *dado)
+
+
+TipoInfo*  retiraDoInicio(Lista *aLista)
+
+
+inteiro  eliminaDoInicio(Lista *aLista)
+
+necessario
+
+1) maurilio
+Adiciona(ElementoLista) nao tem codigo
+AdicionaNoInicio(ElementoLista) slide
+AdicionaNaPosicao(ElementoLista) slide
+AdicionaEmOrdem(ElementoLista) slide
+2) lauro
+Retira() nao tem coidigo
+RetiraDoInicio() slide 2 formas
+RetiraDaPosicao(posicao) slide
+RetiraEspecifico(ElementoLista) nao tem no slide
+3) fristram
+ListaVazia() slide
+Posicao(ElementoFila)
+Contem(ElementoFila)
+CriaLista() slide
+DestroiLista()
+4)  lauro
+cabecalho listaenc.h
+
+5)  lauro
+main
+
+no slide tem 
+
+main tem no slide
