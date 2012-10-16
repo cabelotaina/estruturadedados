4fbc8f36ba3265caaee5944663a0c105414adc1d
Maurilio Atila <cabelotaina@gmail.com>
2012-10-16 18:57:30 -0300

    tipoinfo.h

---
+++ trabalho5/tipoinfo.h
@@ -0,0 +1,19 @@
+#ifndef _TIPO_INFO_
+#define _TIPO_INFO_
+
+#include <string>
+using namespace std;
+class TipoInfo{
+
+public:
+        int telefone;
+        string nome;
+
+      bool maior(string *dado);
+      bool menor(string *dado);
+      bool igual(string *dado);
+	
+
+
+};
+#endif
