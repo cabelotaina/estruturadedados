6a7601d4e84567dc16ecb7fc2ece2fabea74907e
Maurilio Atila <cabelotaina@gmail.com>
2012-10-19 21:08:41 -0300

    fila.cc, fila.h elementofila.h estao corretos falta o main

---
+++ trabalho5c/fila.h
@@ -0,0 +1,26 @@
+// Neste arquivo esta a divisao do trabalho para Maurilio, Fristram e Lauro
+
+#ifndef _FILA_ENCADEADA_
+#define _FILA_ENCADEADA_
+#include "tipoinfo.h"
+#include "elementofila.h"
+
+#define ERROFIVAZIA -1
+#define ERROPOSICAO -2
+#define ERROFILACHEIA -3
+class Fila{
+
+public:
+	ElementoFila *inicio;
+	ElementoFila *fim;
+	int tamanho;
+Fila();
+~Fila();
+int adiciona(Fila *aFila, TipoInfo *dado);
+int retira(Fila *aFila); 
+TipoInfo* retiraDoInicio(Fila *aFila); 
+bool filaVazia(Fila *aFila); 
+Fila* criaFila(); 
+void destroiFila(Fila *aFila);
+};
+#endif
